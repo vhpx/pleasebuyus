215106c20f7d4e281733290a9ee93b6b99805b0f
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-03-12 00:42:53 +0700

    Added Cart functionalities

---
+++ hooks/useCart.js
@@ -0,0 +1,104 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { useUser } from './useUser';
+
+const CartContext = createContext();
+
+export const CartProvider = (props) => {
+    const [items, setItems] = useState([]);
+    const [initialized, setInitialized] = useState(false);
+
+    const { user } = useUser();
+
+    const initialize = () => {
+        if (initialized) return;
+        setInitialized(true);
+    };
+
+    useEffect(() => {
+        if (!initialized) return;
+    }, [user, initialized]);
+
+    const getTotal = () => {
+        return items.reduce(
+            (total, item) => total + item.price * item.quantity,
+            0
+        );
+    };
+
+    const getTotalItems = () => {
+        return items.reduce((total, item) => total + item.quantity, 0);
+    };
+
+    const addItem = (item) => {
+        const newItems = [...items];
+        const itemIndex = newItems.findIndex((i) => i.id === item.id);
+
+        if (itemIndex === -1) newItems.push({ ...item, quantity: 1 });
+        else newItems[itemIndex].quantity++;
+
+        setItems(newItems);
+        // toast.success(`${item.name} added to cart`, {
+        //     position: toast.POSITION.BOTTOM_RIGHT,
+        // });
+    };
+
+    const removeItem = (itemId, itemName) => {
+        const newItems = [...items];
+        const itemIndex = newItems.findIndex((i) => i.id === itemId);
+
+        if (newItems[itemIndex].quantity === 1) {
+            newItems.splice(itemIndex, 1);
+        } else {
+            newItems[itemIndex].quantity--;
+        }
+
+        setItems(newItems);
+        // toast.error(`${itemName ?? 'An item'} has been removed from cart`, {
+        //     position: toast.POSITION.BOTTOM_LEFT,
+        // });
+    };
+
+    const removeAllItems = (itemId, itemName) => {
+        setItems((prevItems) => prevItems.filter((i) => i.id !== itemId));
+        // toast.error(
+        //     `${
+        //         itemName ?? 'All items of this type'
+        //     } have been removed from cart`,
+        //     {
+        //         position: toast.POSITION.BOTTOM_LEFT,
+        //     }
+        // );
+    };
+
+    const clearCart = () => {
+        setItems([]);
+        toast.info('Cart cleared', {
+            position: toast.POSITION.BOTTOM_LEFT,
+        });
+    };
+
+    const values = {
+        initialize,
+
+        items,
+
+        addItem,
+        removeItem,
+        removeAllItems,
+        clearCart,
+
+        getTotal,
+        getTotalItems,
+    };
+
+    return <CartContext.Provider value={values} {...props} />;
+};
+
+export const useCart = () => {
+    const context = useContext(CartContext);
+    if (context === undefined) {
+        throw new Error(`useCart must be used within a CartProvider.`);
+    }
+    return context;
+};
