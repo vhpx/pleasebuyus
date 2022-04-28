d60a3e68176361c7267837233f1dcba0494b7c7b
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-28 23:10:56 +0700

    Updated responsive UI for forgot/reset password

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -59,7 +59,7 @@ export default function ResetPasswordPage() {
     };
 
     return (
-        <div className="relative text-black dark:text-white flex h-screen flex-col items-center justify-center bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 dark:from-[#0d324d] dark:to-[#7f5a83] md:bg-gradient-to-r">
+        <div className="relative text-black dark:text-white flex h-screen flex-col items-center md:justify-center bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 dark:from-[#0d324d] dark:to-[#7f5a83] md:bg-gradient-to-r">
             <BetterLink
                 href="/settings"
                 className="m-8 flex items-center space-x-1 rounded-lg bg-white/60 py-3 pl-4 pr-6 font-bold shadow transition duration-300 hover:bg-white hover:shadow-xl dark:bg-zinc-900/60 dark:text-white dark:hover:bg-zinc-900 md:absolute md:top-0 md:left-0"
@@ -68,7 +68,7 @@ export default function ResetPasswordPage() {
                 <div>Back</div>
             </BetterLink>
 
-            <div className="m-16 flex flex-col items-center rounded-lg border bg-white px-4 py-4 shadow dark:border-transparent dark:bg-zinc-800/60 dark:text-white md:px-16">
+            <div className="md:m-16 flex flex-col items-center rounded-lg border bg-white px-4 py-4 shadow dark:border-transparent dark:bg-zinc-800/60 dark:text-white md:px-16">
                 <h1 className="mb-8 flex text-center items-center justify-center border-b py-2 text-xl font-bold dark:border-zinc-500">
                     {success
                         ? 'Password updated successfully'
