b1c0c53a3b63c64ec6265eb8e6b9ecf6110dac37
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-28 17:43:21 +0700

    Updated divider dark mode color

--- components/common/Divider.js
+++ components/common/Divider.js
@@ -1,5 +1,5 @@
 export default function Divider() {
     return (
-        <div className="mt-2 mb-8 h-[1px] w-full bg-zinc-300 dark:bg-zinc-700" />
+        <div className="mt-2 mb-8 h-[1px] w-full bg-zinc-300 dark:bg-zinc-700/70" />
     );
 }
