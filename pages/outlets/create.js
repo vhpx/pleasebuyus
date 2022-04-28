56a49ae37a65b1284d7f834cb13cb8a18047a33d
Thu Nguyen <anhthunguyen020703@gmail.com>
2022-04-28 16:40:30 +0700

    Added Create Outlet page

--- pages/outlets/create.js
+++ pages/outlets/create.js
@@ -1,3 +1,8 @@
+import OutlinedButton from '../../components/buttons/OutlinedButton';
+import Divider from '../../components/common/Divider';
+import Title from '../../components/common/Title';
+import FormInput from '../../components/form/FormInput';
+import FormLabel from '../../components/form/FormLabel';
 import { StoreLayout } from '../../components/layout/layout';
 
 OutletCreationPage.getLayout = (page) => {
@@ -5,5 +10,19 @@ OutletCreationPage.getLayout = (page) => {
 };
 
 export default function OutletCreationPage() {
-    return <div></div>;
+    return (
+        <div className="p-4 md:p-8 lg:p-16">
+            <div className="bg-white dark:bg-zinc-800 rounded-lg p-8">
+                <Title label="Create your outlet" />
+                <Divider />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6">
+                    <FormInput label="Name" />
+                    <FormInput label="Address" />
+                </div>
+                <div className="flex justify-end mt-3">
+                    <OutlinedButton label="Create outlet" />
+                </div>
+            </div>
+        </div>
+    );
 }
