fd5613949d717e8ec986414e30d95549910a2e7b
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-28 21:37:39 +0700

    Added customizable className for Divider

--- components/common/Divider.js
+++ components/common/Divider.js
@@ -1,5 +1,7 @@
-export default function Divider() {
+export default function Divider({ className }) {
     return (
-        <div className="mt-2 mb-8 h-[1px] w-full bg-zinc-300 dark:bg-zinc-700/70" />
+        <div
+            className={`mt-2 mb-8 h-[1px] w-full bg-zinc-300 dark:bg-zinc-700/70 ${className}`}
+        />
     );
 }
