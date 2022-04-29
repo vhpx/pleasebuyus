899c85dd057cf19627fea7f3405a0a8fd29e0810
Thu Nguyen <anhthunguyen020703@gmail.com>
2022-04-29 15:28:16 +0700

    Updated title component

--- components/typography/Title.js
+++ components/typography/Title.js
@@ -1,7 +1,7 @@
-export default function Title(props) {
+export default function Title({label}) {
     return (
         <div className="col-span-full text-3xl font-bold dark:text-white">
-            {props.label}
+            {label}
         </div>
     );
 }
