5f9d0b5d2313f190454037154eda5eb744b8da75
Thu Nguyen <anhthunguyen020703@gmail.com>
2022-04-29 15:32:10 +0700

    Added ImageCard component

---
+++ components/cards/ImageCard.js
@@ -0,0 +1,17 @@
+export default function ImageCard({ name, desc, src, alt }) {
+    return (
+        <div className="h-80 border border-black rounded-lg">
+            <div className=" h-3/4 rounded-t-lg">
+                <img
+                    src={src} 
+                    alt={alt ?? 'an image'}
+                    className="h-full w-full rounded-t-lg"
+                />
+            </div>
+            <div className=" h-1/4 rounded-b-lg m-3">
+                <div className="font-semibold text-xl">{name ?? 'Unnamed'}</div>
+                <div className="text-sm">{desc}</div>
+            </div>
+        </div>
+    );
+}
