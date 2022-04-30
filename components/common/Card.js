dcd309125da012dc42f43f757ad251b9fef7b042
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-30 16:46:34 +0700

    Updated Card color scheme for light/dark mode

--- components/common/Card.js
+++ components/common/Card.js
@@ -1,8 +1,8 @@
 export default function Card({ children, className, disableHoverEffect }) {
     const defaultCss = `bg-zinc-50 ${
         disableHoverEffect ||
-        'hover:bg-white dark:hover:bg-zinc-800 hover:border-zinc-400 dark:hover:border-zinc-500'
-    } dark:bg-zinc-800/70 border dark:border-zinc-700 rounded-lg shadow transition duration-300`;
+        'hover:bg-white dark:hover:bg-zinc-800 hover:border-zinc-300 dark:hover:border-zinc-700'
+    } dark:bg-zinc-800/70 border dark:border-zinc-800 rounded-lg shadow transition duration-300`;
 
     return <div className={`${defaultCss} p-4 ${className}`}>{children}</div>;
 }
