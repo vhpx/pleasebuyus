f2a22ed7ad68a93242d58abe921c501d8deb4236
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-02 20:54:09 +0700

    Added membership page

---
+++ pages/membership/index.js
@@ -0,0 +1,9 @@
+import { StoreLayout } from '../../components/layout/layout';
+
+MembershipPage.getLayout = (page) => {
+    return <StoreLayout>{page}</StoreLayout>;
+};
+
+export default function MembershipPage() {
+    return <div></div>;
+}
