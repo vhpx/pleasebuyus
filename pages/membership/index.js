aa1f7fbbc2d97615e0ac18a5c5a4850172f4d777
Thu Nguyen <anhthunguyen020703@gmail.com>
2022-05-02 23:04:42 +0700

    Added Membership page

--- pages/membership/index.js
+++ pages/membership/index.js
@@ -1,3 +1,7 @@
+import Avatar from '../../components/common/Avatar';
+import Card from '../../components/common/Card';
+import Divider from '../../components/common/Divider';
+import Title from '../../components/common/Title';
 import { StoreLayout } from '../../components/layout/layout';
 
 MembershipPage.getLayout = (page) => {
@@ -5,5 +9,47 @@ MembershipPage.getLayout = (page) => {
 };
 
 export default function MembershipPage() {
-    return <div></div>;
+    return (
+        <div>
+            <div className="p-4 md:p-8 lg:p-16 space-y-8">
+                <div className="bg-white dark:bg-zinc-800/50 p-8 rounded-lg">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 space-x-12">
+                        <div className="">
+                            <div className="flex items-center mb-10">
+                            <Avatar size={100} />
+                                <div className="ml-3">
+                                    <div className=" text-lg font-semibold">
+                                        Nguyen Pham Anh Thu
+                                    </div>
+                                    <div>Silver</div>
+                                </div>
+                            </div>
+                            <Divider />
+                            <div>
+                                <div className="text-2xl font-semibold">
+                                    Silver
+                                </div>
+                                <div>Progress bar</div>
+                                <div className="text-sm">
+                                    Get 122 more points to unlock Gold
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-2">
+                            <Title label="Tier Benefits" />
+                            <Divider/>
+                            <div className="w-3/4 grid grid-cols-5 mt-2 gap-2 text-center">
+                                <Card className="p-2">Member</Card>
+                                <Card className="p-2">Bronze</Card>
+                                <Card className="p-2">Silver</Card>
+                                <Card className="p-2">Gold</Card>
+                                <Card className="p-2">Platinum</Card>
+                            </div>
+                            <div>Info</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
