4de80de8994982991bca2660787b41bc351a584e
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-02 21:42:16 +0700

    Updated UserDropdown styles

--- components/dropdown/UserDropdown.js
+++ components/dropdown/UserDropdown.js
@@ -38,7 +38,7 @@ export default function UserDropdown({
                 <div className="my-1 flex w-64 flex-col rounded-lg bg-white/50 shadow backdrop-blur dark:bg-zinc-700/50">
                     <BetterLink
                         href="/outlets"
-                        className="px-4 py-2 font-semibold hover:bg-zinc-200/20 dark:hover:bg-zinc-700/40"
+                        className="rounded-t-lg px-4 py-2 font-semibold hover:bg-zinc-200/20 dark:hover:bg-zinc-700/40"
                     >
                         Outlets
                     </BetterLink>
@@ -52,7 +52,7 @@ export default function UserDropdown({
 
                     <BetterLink
                         href="/history"
-                        className="rounded-t-lg px-4 py-2 font-semibold hover:bg-zinc-200/20 dark:hover:bg-zinc-700/40"
+                        className="px-4 py-2 font-semibold hover:bg-zinc-200/20 dark:hover:bg-zinc-700/40"
                     >
                         Purchase History
                     </BetterLink>
