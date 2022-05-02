43c9b2488020fb75ed6a9d9fde26d21caef26768
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-02 20:36:52 +0700

    Updated ImageCard with advanced parameters

--- components/cards/ImageCard.js
+++ components/cards/ImageCard.js
@@ -1,25 +1,40 @@
 import Card from '../common/Card';
 
-export default function ImageCard({ name, desc, imageUrl, alt }) {
+export default function ImageCard({ hideContent, name, desc, imageUrl, alt }) {
     return (
-        <Card className="p-0 mb-7">
-            <div className="h-48 rounded-t-lg">
+        <Card className="p-0">
+            <div
+                className={`h-48 min-w-[12rem] w-full ${
+                    hideContent ? 'rounded-lg' : 'rounded-t-lg'
+                }`}
+            >
                 {imageUrl ? (
                     /* eslint-disable-next-line @next/next/no-img-element */
                     <img
                         src={imageUrl}
                         alt={alt ?? 'an image'}
-                        className="h-full w-full rounded-t-lg"
+                        className={`h-full w-full ${
+                            hideContent ? 'rounded-lg' : 'rounded-t-lg'
+                        }`}
                         style={{ objectFit: 'cover' }}
                     />
                 ) : (
-                    <div className="h-full w-full bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 dark:from-green-300/70 dark:via-blue-500/70 dark:to-purple-600/70 rounded-t-lg" />
+                    <div
+                        className={`h-full w-full bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 dark:from-green-300/70 dark:via-blue-500/70 dark:to-purple-600/70 ${
+                            hideContent ? 'rounded-lg' : 'rounded-t-lg'
+                        }`}
+                    />
                 )}
             </div>
-            <div className="rounded-b-lg m-3">
-                <div className="font-semibold text-xl">{name ?? 'Unnamed'}</div>
-                <div className="text-sm">{desc}</div>
-            </div>
+
+            {hideContent || (
+                <div className="rounded-b-lg m-3">
+                    <div className="font-semibold text-xl">
+                        {name ?? 'Unnamed'}
+                    </div>
+                    <div className="text-sm">{desc}</div>
+                </div>
+            )}
         </Card>
     );
 }
