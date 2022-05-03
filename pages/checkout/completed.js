b96673390ec3da9fa67f0cddea0fe52fa3091831
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-03 11:00:24 +0700

    Updated checkout routing

---
+++ pages/checkout/completed.js
@@ -0,0 +1,9 @@
+import { StoreLayout } from '../../components/layout/layout';
+
+CheckoutCompletedPage.getLayout = (page) => {
+    return <StoreLayout>{page}</StoreLayout>;
+};
+
+export default function CheckoutCompletedPage() {
+    return <div></div>;
+}
