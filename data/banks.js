02099c3335d505c62594aa021122e817389a87b1
Thu Nguyen <anhthunguyen020703@gmail.com>
2022-05-03 10:43:30 +0700

    Added bank data

--- data/banks.js
+++ data/banks.js
@@ -1,1 +1,172 @@
-export const banks = [];
+export const banks = [
+    {
+        name: 'Asia Commercial Joint Stock Bank',
+        short_name: 'ACB',
+        code: 'ACB',
+    },
+    {
+        name: 'Tien Phong Commercial Joint Stock Bank',
+        short_name: 'TPBank',
+        code: 'TPBank',
+    },
+    {
+        name: 'Dong A Commercial Joint Stock Bank',
+        short_name: 'Đông Á Bank',
+        code: 'DAB',
+    },
+    {
+        name: 'Southeast Asia Commercial Joint Stock Bank',
+        short_name: 'SeABank',
+        code: 'SeABank',
+    },
+    {
+        name: 'An Binh Commercial Joint Stock Bank',
+        short_name: 'ABBANK',
+        code: 'ABBANK',
+    },
+    {
+        name: 'Bac A Commercial Joint Stock Bank',
+        short_name: 'BacABank',
+        code: 'BacABank',
+    },
+    {
+        name: 'Vietcapital Commercial Joint Stock Bank',
+        short_name: 'VietCapitalBank',
+        code: 'VietCapitalBank',
+    },
+    {
+        name: 'Vietnam Maritime Joint – Stock Commercial Bank',
+        short_name: 'MSB',
+        code: 'MSB',
+    },
+    {
+        name: 'VietNam Technological and Commercial Joint Stock Bank',
+        short_name: 'Techcombank',
+        code: 'TCB',
+    },
+    {
+        name: 'Kien Long Commercial Joint Stock Bank',
+        short_name: 'KienLongBank',
+        code: 'KienLongBank',
+    },
+    {
+        name: 'Nam A Comercial Join Stock Bank',
+        short_name: 'Nam A Bank',
+        code: 'Nam A Bank',
+    },
+    {
+        name: 'National Citizen Commercial Joint Stock Bank',
+        short_name: 'National Citizen Bank',
+        code: 'NCB',
+    },
+    {
+        name: 'Tien Phong Commercial Joint Stock Bank',
+        short_name: 'TPBank',
+        code: 'TPBank',
+    },
+    {
+        name: 'Vietnam Prosperity Joint Stock Commercial Bank',
+        short_name: 'VPBank',
+        code: 'VPBank',
+    },
+    {
+        name: 'Ho Chi Minh City Housing Development Bank',
+        short_name: 'HDBank',
+        code: 'HDBank',
+    },
+    {
+        name: 'Orient Commercial Joint Stock Bank',
+        short_name: 'OCB',
+        code: 'OCB',
+    },
+    {
+        name: 'Military Commercial Joint Stock Bank',
+        short_name: 'Military Bank',
+        code: 'MB',
+    },
+    {
+        name: 'Vietnam Public Joint Stock Commercial Bank',
+        short_name: 'PVcombank',
+        code: 'PVcombank',
+    },
+    {
+        name: 'Vietnam International and Commercial Joint Stock Bank',
+        short_name: 'VIBBank',
+        code: 'VIB',
+    },
+    {
+        name: 'Saigon Bank for Industry and Trade',
+        short_name: 'Saigonbank',
+        code: 'SGB',
+    },
+    {
+        name: 'Saigon – Hanoi Commercial Joint Stock Bank',
+        short_name: 'SHBank',
+        code: 'SHB',
+    },
+    {
+        name: 'Sai Gon Thuong Tin Commercial Joint Stock Bank',
+        short_name: 'Sacombank',
+        code: 'STB',
+    },
+    {
+        name: 'Vietnam Asia Commercial Joint Stock Bank',
+        short_name: 'VietABank',
+        code: 'VAB',
+    },
+    {
+        name: 'Bao Viet Joint Stock Commercial Bank',
+        short_name: 'VIBaoVietBankBBank',
+        code: 'BVB',
+    },
+    {
+        name: 'Joint Stock Commercia Petrolimex Bank',
+        short_name: 'Petrolimex Group Bank',
+        code: 'PG Bank',
+    },
+    {
+        name: 'Vietnam Joint Stock Commercia lVietnam Export Import Bank',
+        short_name: 'Eximbank',
+        code: 'EIB',
+    },
+    {
+        name: 'Joint stock commercial Lien Viet postal bank',
+        short_name: 'LienVietPostBank',
+        code: 'LPB',
+    },
+    {
+        name: 'JSC Bank for Investment and Development of Vietnam',
+        short_name: 'BIDV',
+        code: 'BIDV',
+    },
+    {
+        name: 'Construction Bank',
+        short_name: 'CB',
+        code: 'CB',
+    },
+    {
+        name: 'Ocean Commercial One Member Limited Liability Bank',
+        short_name: 'Oceanbank',
+        code: 'VOceanbankIB',
+    },
+    {
+        name: 'Global Petro Commercial Joint Stock Bank',
+        short_name: 'GPBank',
+        code: 'GPBank',
+    },
+    {
+        name: 'Vietnam Bank for Agriculture and Rural Development',
+        short_name: 'Agribank',
+        code: 'Agribank',
+    },
+    {
+        name: 'Shinhan Vietnam Bank Limited',
+        short_name: 'SHBVN',
+        code: 'SHBVN',
+    },
+    {
+        name: 'The Hongkong and Shanghai Banking Corporation Limited',
+        short_name: 'HSBC',
+        code: 'HSBC',
+    },
+];
