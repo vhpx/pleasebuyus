16a2830d394916da7b6905fb4e21ba56d2e781eb
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-03 19:18:24 +0700

    Updated Membership page UI

--- pages/membership/index.js
+++ pages/membership/index.js
@@ -1,18 +1,127 @@
 import { SparklesIcon } from '@heroicons/react/outline';
+import { CheckCircleIcon, LockClosedIcon } from '@heroicons/react/solid';
+import { useEffect, useState } from 'react';
+import TierCard from '../../components/cards/TierCard';
 import Avatar from '../../components/common/Avatar';
-import Card from '../../components/common/Card';
 import Divider from '../../components/common/Divider';
 import Title from '../../components/common/Title';
 import { StoreLayout } from '../../components/layout/layout';
 import { useUser } from '../../hooks/useUser';
 
 MembershipPage.getLayout = (page) => {
-    return <StoreLayout>{page}</StoreLayout>;
+    return <StoreLayout hideFooter={true}>{page}</StoreLayout>;
 };
 
 export default function MembershipPage() {
     const { userData } = useUser();
 
+    const tiers = [
+        {
+            name: 'Member',
+            color: 'green',
+            points: 0,
+            benefits: ['Access to standard deals'],
+        },
+        {
+            name: 'Silver',
+            color: 'blue',
+            points: 100,
+            benefits: [
+                'Earn 1.5x rewards points as a Silver user',
+                'Get access to deals such as movie screening, flash sales and more',
+            ],
+        },
+        {
+            name: 'Gold',
+            color: 'yellow',
+            points: 500,
+            benefits: [
+                'Earn 2x rewards points as a Gold user',
+                'Get access to deals such as movie screening, flash sales and more',
+                'Priority support',
+            ],
+        },
+        {
+            name: 'Platinum',
+            color: 'sky',
+            points: 1000,
+            benefits: [
+                'Earn 3x rewards points as a Platinum user',
+                'Get access to deals such as movie screening, flash sales and more',
+                'Get access to exclusive deals and offers',
+                'Early access to new features',
+                'Priority support',
+            ],
+        },
+    ];
+
+    const [progressPoints, setProgressPoints] = useState(0);
+
+    const getCurrentTier = () => {
+        if (progressPoints < 100) return tiers[0];
+        if (progressPoints < 500) return tiers[1];
+        if (progressPoints < 1000) return tiers[2];
+        return tiers[3];
+    };
+
+    const getCurrentTierBg = () => {
+        const { name } = getCurrentTier();
+
+        switch (name) {
+            case 'Member':
+                return `bg-green-600/70 dark:bg-green-300`;
+
+            case 'Silver':
+                return `bg-blue-600/70 dark:bg-blue-300`;
+
+            case 'Gold':
+                return `bg-yellow-600/70 dark:bg-yellow-300`;
+
+            case 'Platinum':
+                return `bg-sky-600/70 dark:bg-sky-300`;
+
+            default:
+                return `bg-zinc-600/70 dark:bg-zinc-300`;
+        }
+    };
+
+    const getNextTier = () => {
+        if (progressPoints < 100) return tiers[1];
+        if (progressPoints < 500) return tiers[2];
+        return tiers[3];
+    };
+
+    const getNextTierText = () => {
+        const { name } = getNextTier();
+
+        switch (name) {
+            case 'Silver':
+                return 'text-blue-700 lg:bg-blue-400/20 dark:text-blue-300';
+
+            case 'Gold':
+                return 'text-yellow-700 lg:bg-yellow-400/20 dark:text-yellow-300';
+
+            case 'Platinum':
+                return 'text-sky-700 lg:bg-sky-400/20 dark:text-sky-300';
+
+            default:
+                return 'text-zinc-700 lg:bg-zinc-400/20 dark:text-zinc-300';
+        }
+    };
+
+    const [selectedTier, setSelectedTier] = useState(0);
+
+    useEffect(() => {
+        const getCurrentTier = () => {
+            if (progressPoints < 100) return 0;
+            if (progressPoints < 500) return 1;
+            if (progressPoints < 1000) return 2;
+            return 3;
+        };
+
+        const tier = getCurrentTier();
+        setSelectedTier(tier);
+    }, [progressPoints]);
     return (
         <div>
             <div className="p-4 md:p-8 lg:p-16 space-y-8">
@@ -32,95 +141,107 @@ export default function MembershipPage() {
                                     <div className="flex items-center space-x-1 text-zinc-500 dark:text-zinc-300">
                                         <SparklesIcon className="w-4 h-4 md:w-6 md:h-6" />
                                         <div className="text-sm md:text-lg font-semibold">
-                                            Silver
+                                            {progressPoints} points
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <Divider />
                             <div>
-                                <div className="flex justify-between text-zinc-600/70 dark:text-zinc-300 items-end mb-1">
+                                <div
+                                    className={`flex justify-between text-${
+                                        getCurrentTier().color
+                                    }-600/70 dark:text-${
+                                        getCurrentTier().color
+                                    }-300 items-end mb-1`}
+                                >
                                     <div className="text-2xl font-bold">
-                                        Silver
+                                        {getCurrentTier().name}
                                     </div>
                                     <span className="text-sm font-medium dark:text-white">
-                                        45%
+                                        {getNextTier().points} points
                                     </span>
                                 </div>
 
                                 <div className="w-full bg-zinc-200 rounded-full h-2.5 dark:bg-zinc-700">
                                     <div
-                                        className="bg-zinc-600/70 dark:bg-zinc-300 h-2.5 rounded-full"
+                                        className={`${getCurrentTierBg()} h-2.5 rounded-full`}
                                         style={{
-                                            width: '45%',
+                                            width: `${
+                                                progressPoints >=
+                                                getNextTier().points
+                                                    ? '100'
+                                                    : (progressPoints /
+                                                          getNextTier()
+                                                              .points) *
+                                                      100
+                                            }%`,
                                         }}
                                     />
                                 </div>
 
-                                <div className="mt-4 text-sm">
-                                    Get{' '}
-                                    <span className="font-semibold">122</span>{' '}
-                                    more points to unlock{' '}
-                                    <span className="ml-1 font-semibold text-yellow-700 px-4 py-1 rounded-lg bg-yellow-500/20 dark:text-yellow-300">
-                                        Gold
-                                    </span>
-                                </div>
+                                {getCurrentTier().name !=
+                                    getNextTier().name && (
+                                    <div className="mt-4 text-sm">
+                                        Get{' '}
+                                        <span className="font-semibold">
+                                            {getNextTier().points -
+                                                progressPoints}{' '}
+                                            points
+                                        </span>{' '}
+                                        more points to unlock{' '}
+                                        <span
+                                            className={`ml-1 font-semibold lg:px-4 lg:py-1 rounded-lg ${getNextTierText()}`}
+                                        >
+                                            {getNextTier().name}
+                                        </span>
+                                    </div>
+                                )}
                             </div>
                         </div>
 
                         <div className="mt-8 lg:mt-0 col-span-2">
-                            <Title label="Tier Benefits" />
+                            <Title label="Membership Tiers" />
                             <Divider />
-                            <div className="lg:w-4/5 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 mt-2 gap-2 font-semibold text-center">
-                                <Card className="cursor-pointer p-2 text-green-600 hover:text-green-700 bg-green-400/20 hover:bg-green-400/30 border-green-600/20 hover:border-green-600/50 dark:text-green-300 dark:hover:text-green-100 dark:bg-green-200/20 dark:hover:bg-green-400/30 dark:border-green-300/20 dark:hover:border-green-300/50">
-                                    Member
-                                </Card>
-                                <Card className="cursor-pointer p-2 text-yellow-700 hover:text-yellow-800 bg-yellow-500/20 hover:bg-yellow-500/30 border-yellow-600/20 hover:border-yellow-600/50 dark:text-yellow-500 dark:hover:text-yellow-300 dark:bg-yellow-500/20 dark:hover:bg-yellow-500/30 dark:border-yellow-600/20 dark:hover:border-yellow-600/50">
-                                    Bronze
-                                </Card>
-                                <Card className="cursor-pointer p-2 text-zinc-600 hover:text-zinc-700 bg-zinc-400/20 hover:bg-zinc-400/30 border-zinc-500/20 hover:border-zinc-500/50 dark:text-zinc-300 dark:hover:text-zinc-100 dark:bg-zinc-200/20 dark:hover:bg-zinc-400/30 dark:border-zinc-200/20 dark:hover:border-zinc-200/50">
-                                    Silver
-                                </Card>
-                                <Card className="cursor-pointer p-2 text-yellow-600 hover:text-yellow-700 bg-yellow-400/20 hover:bg-yellow-400/30 border-yellow-600/20 hover:border-yellow-600/50 dark:text-yellow-300 dark:hover:text-yellow-100 dark:bg-yellow-200/20 dark:hover:bg-yellow-400/30 dark:border-yellow-300/20 dark:hover:border-yellow-300/50">
-                                    Gold
-                                </Card>
-                                <Card className="cursor-pointer p-2 md:col-span-full lg:col-span-1 text-sky-600 hover:text-sky-700 bg-sky-400/20 hover:bg-sky-400/30 border-sky-500/20 hover:border-sky-500/50 dark:text-sky-300 dark:hover:text-sky-100 dark:bg-sky-200/20 dark:hover:bg-sky-400/30 dark:border-sky-300/20 dark:hover:border-sky-300/50">
-                                    Platinum
-                                </Card>
+
+                            <div className="lg:w-4/5 mb-8 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 mt-2 gap-2 text-center">
+                                {tiers.map((tier, index) => (
+                                    <button
+                                        key={tier.name}
+                                        onClick={() => setSelectedTier(index)}
+                                    >
+                                        <TierCard
+                                            tier={tier}
+                                            selected={index === selectedTier}
+                                        />
+                                    </button>
+                                ))}
                             </div>
-                            <div>
-                                <div className="text-xl">Member</div>
-                                <div>
-                                    Pleasebuy.us is a site dedicated to helping
-                                    people buy things they want. For every
-                                    purchase, earn points to receive discounts
-                                    and exclusive offers. You'll unlock new
-                                    tiers as you go, each with its own set of
-                                    rewards.
-                                </div>
-                                <div className="text-xl">Bronze</div>
-                                <div>
-                                    Get access to random deals such as movie
-                                    screening, flash sales and more
-                                </div>
-                                <div className="text-xl">Silver</div>
-                                <div>
-                                    Earn 1.5x rewards points as a Silver user
-                                </div>
-                                <div className="text-xl">Gold</div>
-                                <div>
-                                    Earn 2x rewards points as a Gold user Get
-                                    access to exclusive deals such as movie
-                                    screening, flash sales and more
-                                </div>
-                                <div className="text-xl">Platinum</div>
-                                <div>
-                                    Earn 3x rewards points as a Platinum user
-                                    Get access to exclusive deals such as movie
-                                    screening, flash sales and more Enjoy
-                                    exclusive offers from Pleasebuy.us partner
-                                </div>
+
+                            <div className="text-sm lg:text-base flex flex-col items-center space-y-2 lg:flex-row lg:space-x-2 lg:space-y-0 text-center">
+                                <Title label="Tier Benefits" />
+                                {tiers[selectedTier].name ===
+                                    getCurrentTier().name && (
+                                    <div className="font-semibold px-4 py-1 rounded-lg bg-zinc-100 dark:bg-zinc-700/70 w-fit text-zinc-500 dark:text-zinc-300">
+                                        You are currently on this tier
+                                    </div>
+                                )}
                             </div>
+                            <Divider />
+
+                            {tiers[selectedTier].benefits.map(
+                                (benefit, index) => (
+                                    <div
+                                        key={index}
+                                        className={`flex items-center mb-2 text-${tiers[selectedTier].color}-600 dark:text-${tiers[selectedTier].color}-300`}
+                                    >
+                                        <CheckCircleIcon className="w-4 flex-none h-4 md:w-8 md:h-8 mr-2" />
+                                        <div className="md:text-lg font-semibold">
+                                            {benefit}
+                                        </div>
+                                    </div>
+                                )
+                            )}
                         </div>
                     </div>
                 </div>
