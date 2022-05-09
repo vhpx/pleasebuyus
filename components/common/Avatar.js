967f1f55fd92cc63e1c98cc8513277e4c640b587
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-09 09:44:20 +0700

    Updated Default Avatar with overridable settings

--- components/common/Avatar.js
+++ components/common/Avatar.js
@@ -2,13 +2,15 @@ import Image from 'next/image';
 import { useUser } from '../../hooks/useUser';
 import DefaultAvatar from './DefaultAvatar';
 
-export default function Avatar({ size = 40, url }) {
+export default function Avatar({ size = 40, url, hideDefault, alt }) {
     const defaultCss = 'rounded-lg';
 
     const { userData } = useUser();
 
-    const avatarUrl = url ?? userData?.['avatar_url'];
-    const displayName = userData?.['display_name'] ?? 'Profile avatar';
+    const avatarUrl = url ?? (hideDefault ? '' : userData?.['avatar_url']);
+    const displayName = url
+        ? alt
+        : userData?.['display_name'] ?? 'Profile avatar';
 
     return (
         <>
@@ -20,6 +22,8 @@ export default function Avatar({ size = 40, url }) {
                     height={size}
                     className={defaultCss}
                 />
+            ) : hideDefault ? (
+                <div className="h-32 w-32 bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 dark:from-green-300/70 dark:via-blue-500/70 dark:to-purple-600/70 rounded-lg" />
             ) : (
                 <DefaultAvatar size={size} />
             )}
