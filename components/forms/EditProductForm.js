9463fcac24715fd2b155cd7db593438b0ed3a5f1
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-09 11:25:16 +0700

    Added Product featured image to Product form

--- components/forms/EditProductForm.js
+++ components/forms/EditProductForm.js
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { supabase } from '../../utils/supabase-client';
+import Avatar from '../common/Avatar';
+import Divider from '../common/Divider';
 import FormInput from '../form/FormInput';
+import FormLabel from '../form/FormLabel';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function EditProductForm({
     user,
@@ -10,6 +14,8 @@ export default function EditProductForm({
     closeModal,
     setter,
 }) {
+    const [uploading, setUploading] = useState(false);
+
     const [productName, setProductName] = useState(currentProduct?.name || '');
     const [productDescription, setProductDescription] = useState(
         currentProduct?.description || ''
@@ -20,6 +26,68 @@ export default function EditProductForm({
     const [productAvatarUrl, setProductAvatarUrl] = useState(
         currentProduct?.avatar_url || ''
     );
+
+    const downloadImage = (path) => {
+        try {
+            const { publicURL, error } = supabase.storage
+                .from('avatars')
+                .getPublicUrl(path);
+
+            if (error) {
+                throw error;
+            }
+
+            return publicURL;
+        } catch (error) {
+            console.log('Error downloading image: ', error.message);
+        }
+    };
+
+    const uploadAvatar = async (event) => {
+        try {
+            setUploading(true);
+
+            if (!event.target.files || event.target.files.length === 0)
+                throw new Error('You must select an image to upload.');
+
+            if (!currentProduct) throw new Error('Product not found.');
+
+            const file = event.target.files[0];
+            const fileExt = file.name.split('.').pop();
+            const fileName = `${uuidv4()}.${fileExt}`;
+            const filePath = `${fileName}`;
+
+            let { error: uploadError } = await supabase.storage
+                .from('avatars')
+                .upload(filePath, file);
+
+            if (uploadError) {
+                throw uploadError;
+            }
+
+            const avatarUrl = downloadImage(filePath);
+
+            const { error } = await supabase
+                .from('products')
+                .update({ avatar_url: avatarUrl }, { returning: 'minimal' })
+                .eq('id', currentProduct?.id);
+
+            if (error) throw error;
+            setProductAvatarUrl(avatarUrl);
+            setter((prevState) =>
+                prevState.map((product) => {
+                    if (product.id === currentProduct.id) {
+                        return { ...product, avatar_url: avatarUrl };
+                    }
+                    return product;
+                })
+            );
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setUploading(false);
+        }
+    };
 
     const handleSubmit = async () => {
         try {
@@ -131,6 +199,44 @@ export default function EditProductForm({
 
     return (
         <>
+            {currentProduct && (
+                <div>
+                    <div className="flex flex-col mb-8 items-center justify-center space-y-4">
+                        <div className="flex-0 relative">
+                            <Avatar
+                                url={productAvatarUrl}
+                                size={140}
+                                hideDefault={true}
+                                alt={currentProduct.name}
+                            />
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center">
+                            <FormLabel
+                                className="mb-0"
+                                id="avatar"
+                                label={
+                                    uploading
+                                        ? 'Uploading...'
+                                        : 'Upload a featured image for your product'
+                                }
+                            />
+
+                            <input
+                                className="block cursor-pointer rounded-lg border border-zinc-300 bg-white transition duration-300 placeholder:text-black hover:border-zinc-400 hover:bg-zinc-200/50 focus:border-transparent focus:outline-none dark:border-zinc-700/50 dark:bg-zinc-900/70 dark:text-white dark:placeholder:text-white dark:hover:border-zinc-700 dark:hover:bg-zinc-800"
+                                aria-describedby="user_avatar_help"
+                                id="avatar"
+                                type="file"
+                                accept="image/*"
+                                onChange={uploadAvatar}
+                                disabled={uploading}
+                            />
+                        </div>
+                    </div>
+                    <Divider />
+                </div>
+            )}
+
             <FormInput
                 label="Name"
                 id="product-name"
