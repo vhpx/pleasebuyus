ce1f4245c3a5bccfe9e487933a114eebfb98e3a5
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-14 15:19:15 +0700

    Added Admins Manager page

---
+++ components/dashboard/AdminsTable.js
@@ -0,0 +1,173 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../../utils/supabase-client';
+import { toast } from 'react-toastify';
+import LoadingIndicator from '../common/LoadingIndicator';
+
+export default function AdminsTable() {
+    const [admins, setAdmins] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchAdmins = async () => {
+            try {
+                setLoading(true);
+
+                const { data, error } = await supabase
+                    .from('admins')
+                    .select('users (*)')
+                    .order('created_at', { ascending: false });
+
+                if (error) throw error;
+                setAdmins(data);
+            } catch (error) {
+                toast.error(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchAdmins();
+    }, []);
+
+    return loading ? (
+        <div className="text-center">
+            <LoadingIndicator svgClassName="w-8 h-8" />
+        </div>
+    ) : (
+        <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-zinc-200 dark:border-zinc-700 sm:rounded-lg">
+                        <table className="min-w-full divide-y divide-zinc-200 dark:divide-zinc-700">
+                            <thead className="bg-white dark:bg-zinc-800/50">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider"
+                                    >
+                                        Name
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider"
+                                    >
+                                        Phone number
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider"
+                                    >
+                                        Birthday
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider"
+                                    >
+                                        Gender
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="relative px-6 py-3"
+                                    >
+                                        <span className="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white dark:bg-zinc-800/50 divide-y divide-zinc-200 dark:divide-zinc-700">
+                                {admins.map((user) => (
+                                    <tr key={user?.users?.email}>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="flex items-center">
+                                                <div className="flex-shrink-0 h-10 w-10">
+                                                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                                                    {user?.users?.avatar_url ? (
+                                                        <div className="aspect-square rounded-lg">
+                                                            {/* eslint-disable-next-line @next/next/no-img-element */}
+                                                            <img
+                                                                className="h-10 w-10 rounded-lg"
+                                                                src={
+                                                                    user?.users
+                                                                        ?.avatar_url
+                                                                }
+                                                                alt={
+                                                                    user?.users
+                                                                        ?.email
+                                                                }
+                                                            />
+                                                        </div>
+                                                    ) : (
+                                                        <div className="aspect-square w-full bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 dark:from-green-300/70 dark:via-blue-500/70 dark:to-purple-600/70 rounded-lg" />
+                                                    )}
+                                                </div>
+                                                <div className="ml-4">
+                                                    <div className="text-sm font-medium text-zinc-900 dark:text-zinc-200">
+                                                        {user?.users?.name ||
+                                                            '-'}
+                                                    </div>
+                                                    <div className="text-sm text-zinc-500 dark:text-zinc-400">
+                                                        {user?.users?.email}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="text-sm text-zinc-900 dark:text-zinc-200">
+                                                {user?.users?.phone_number ||
+                                                    '-'}
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="text-sm text-zinc-900 dark:text-zinc-200">
+                                                {user?.users?.birthday
+                                                    ? new Date(
+                                                          user?.users?.birthday
+                                                      ).toLocaleDateString(
+                                                          'en-US',
+                                                          {
+                                                              month: 'long',
+                                                              day: 'numeric',
+                                                              year: 'numeric',
+                                                          }
+                                                      )
+                                                    : '-'}
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            {user?.users?.gender ? (
+                                                user?.users?.gender ==
+                                                'male' ? (
+                                                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 dark:bg-blue-300/20 text-blue-800 dark:text-blue-300">
+                                                        Male
+                                                    </span>
+                                                ) : user?.users?.gender ==
+                                                  'female' ? (
+                                                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 dark:bg-red-300/20 text-red-800 dark:text-red-300">
+                                                        Female
+                                                    </span>
+                                                ) : (
+                                                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-purple-100 dark:bg-purple-300/20 text-purple-800 dark:text-purple-300">
+                                                        Other
+                                                    </span>
+                                                )
+                                            ) : (
+                                                '-'
+                                            )}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a
+                                                href="#"
+                                                className="text-blue-600 dark:text-blue-300 hover:text-blue-900 dark:hover:text-blue-200"
+                                            >
+                                                Revoke admin
+                                            </a>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ pages/dashboard/admins.js
@@ -0,0 +1,39 @@
+import Title from '../../components/common/Title.js';
+import AdminsTable from '../../components/dashboard/AdminsTable.js';
+import { SidebarLayout } from '../../components/layout/layout.js';
+import { RequireAuth, useUser } from '../../hooks/useUser';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+
+AdminsDashboardPage.getLayout = (page) => {
+    return <SidebarLayout>{page}</SidebarLayout>;
+};
+
+export default function AdminsDashboardPage() {
+    RequireAuth();
+
+    const router = useRouter();
+
+    const { userData } = useUser();
+    const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+        if (!userData) return;
+        if (!userData?.isAdmin) {
+            toast.error('You are not authorized to view this page.');
+            router.replace('/');
+        } else {
+            setInitialized(true);
+        }
+    }, [userData, router]);
+
+    return initialized ? (
+        <div className="p-4 md:p-8 lg:p-16">
+            <Title label="Admins Manager" className="mb-4" />
+            <AdminsTable />
+        </div>
+    ) : (
+        <div></div>
+    );
+}
