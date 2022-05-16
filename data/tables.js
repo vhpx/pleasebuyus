87020f43a64864cd5c62acc0af245b5d66c79f51
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-16 15:16:46 +0700

    Added Query generator

---
+++ data/tables.js
@@ -0,0 +1,124 @@
+const tableNames = [
+    'users',
+    'memberships',
+    'admins',
+    'addresses',
+    'banks',
+    'bank_cards',
+    'user_cards',
+    'outlets',
+    'outlet_categories',
+    'products',
+    'wishlisted_products',
+    'bills',
+    'bill_products',
+    'coupons',
+    'bill_coupons',
+];
+
+const tables = [
+    {
+        name: 'users',
+        columns: [
+            'id',
+            'name',
+            'email',
+            'phone_number',
+            'birthday',
+            'gender',
+            'avatar_url',
+            'created_at',
+        ],
+    },
+    {
+        name: 'memberships',
+        columns: ['user_id', 'redeemable_pts', 'progress_pts'],
+    },
+    {
+        name: 'admins',
+        columns: ['user_id', 'created_at'],
+    },
+    {
+        name: 'addresses',
+        columns: [
+            'id',
+            'user_id',
+            'name',
+            'country',
+            'province',
+            'city',
+            'street_info',
+            'created_at',
+        ],
+    },
+    {
+        name: 'banks',
+        columns: ['code', 'name', 'short_name', 'created_at'],
+    },
+    {
+        name: 'bank_cards',
+        columns: ['bank_code', 'card_number', 'PIN', 'owner_id', 'created_at'],
+    },
+    {
+        name: 'user_cards',
+        columns: ['id', 'user_id', 'bank_code', 'card_number', 'created_at'],
+    },
+    {
+        name: 'outlets',
+        columns: [
+            'id',
+            'owner_id',
+            'name',
+            'address',
+            'avatar_url',
+            'created_at',
+        ],
+    },
+    {
+        name: 'outlet_categories',
+        columns: ['id', 'outlet_id', 'name', 'created_at'],
+    },
+    {
+        name: 'products',
+        columns: [
+            'id',
+            'outlet_id',
+            'name',
+            'description',
+            'price',
+            'avatar_url',
+            'category_id',
+            'created_at',
+        ],
+    },
+    {
+        name: 'wishlisted_products',
+        columns: ['user_id', 'product_id', 'created_at'],
+    },
+    {
+        name: 'bills',
+        columns: [
+            'id',
+            'customer_id',
+            'card_id',
+            'address_id',
+            'outlet_id',
+            'total',
+            'created_at',
+        ],
+    },
+    {
+        name: 'bill_products',
+        columns: ['bill_id', 'product_id', 'amount'],
+    },
+    {
+        name: 'coupons',
+        columns: ['id', 'code', 'name', 'value', 'use_ratio', 'created_at'],
+    },
+    {
+        name: 'bill_coupons',
+        columns: ['bill_id', 'coupon_id'],
+    },
+];
+
+export { tableNames, tables };
---
+++ pages/query.js
@@ -0,0 +1,126 @@
+import { useEffect, useState } from 'react';
+import Divider from '../components/common/Divider';
+import Title from '../components/common/Title';
+import { StoreLayout } from '../components/layout/layout';
+import { tableNames, tables } from '../data/tables';
+import { supabase } from '../utils/supabase-client';
+import { toast } from 'react-toastify';
+import LoadingIndicator from '../components/common/LoadingIndicator';
+import { ClipboardCopyIcon } from '@heroicons/react/outline';
+import { useUser } from '../hooks/useUser';
+import { Prism } from '@mantine/prism';
+
+QueryPage.getLayout = (page) => {
+    return <StoreLayout>{page}</StoreLayout>;
+};
+
+const demoCode = `import { Button } from '@mantine/core';
+
+function Demo() {
+  return <Button>Hello</Button>
+}`;
+
+export default function QueryPage() {
+    const { userData } = useUser();
+
+    const [tablesData, setTablesData] = useState([]);
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        try {
+            if (!userData) return;
+            if (
+                !(userData.isAdmin && userData.email === 'phucvo0708@gmail.com')
+            ) {
+                toast.error('You are not authorized to access this page.');
+                return;
+            }
+
+            // for each table name, fetch the data
+            const tablePromises = tableNames.map(async (tableName) => {
+                const { data, error } = await supabase
+                    .from(tableName)
+                    .select('*');
+
+                if (error) throw error;
+
+                return {
+                    name: tableName,
+                    data,
+                };
+            });
+
+            Promise.all(tablePromises).then((results) => {
+                setTablesData(results);
+            });
+        } catch (err) {
+            toast.error(err);
+        }
+    }, [userData]);
+
+    useEffect(() => {
+        if (!tablesData || !tablesData.length) return;
+
+        let response =
+            '-- By default, Supabase uses the `public` database.\n\n';
+
+        for (let i = 0; i < tablesData.length; i++) {
+            const table = tablesData[i];
+            const tableColumns = tables.find(
+                (t) => t.name === table.name
+            ).columns;
+
+            const tableName = table.name;
+            const tableData = table.data;
+
+            response += `-- Populate data for the ${tableName} table.\n`;
+            response += `INSERT INTO\n\tpublic.${tableName}(${tableColumns.join(
+                ', '
+            )}) VALUES\n`;
+
+            for (let j = 0; j < tableData.length; j++) {
+                const row = tableData[j];
+
+                response += `\t\t(${tableColumns
+                    .map((c) =>
+                        row[c] === 'null' || row[c] === null || row[c] === ''
+                            ? 'NULL'
+                            : typeof row[c] === 'string'
+                            ? `'${row[c].replace(/'/g, "''")}'`
+                            : row[c]
+                    )
+                    .join(', ')})`;
+
+                if (j < tableData.length - 1) response += ',\n';
+            }
+
+            response += ';';
+            if (i < tablesData.length - 1) response += '\n\n';
+        }
+
+        console.log(response);
+        setQuery(response);
+    }, [tablesData]);
+
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text);
+        toast.success('Copied to clipboard');
+    };
+
+    return (
+        <div className="p-4 md:p-8 lg:p-16 space-y-8">
+            <div className="bg-white dark:bg-zinc-800/50 p-8 rounded-lg">
+                <Title label="Database Insert Queries" />
+                <Divider />
+
+                {query ? (
+                    <Prism language="sql">{query}</Prism>
+                ) : (
+                    <div className="w-full text-center">
+                        <LoadingIndicator svgClassName="w-8 h-8" />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
