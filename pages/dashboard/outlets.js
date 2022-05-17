83cab0285eb3822d4b3114fcf374131ddf70c008
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-17 15:44:45 +0700

    Added new graphs for outlets in Admin Dashboard

--- pages/dashboard/outlets.js
+++ pages/dashboard/outlets.js
@@ -9,6 +9,10 @@ import { PlusIcon } from '@heroicons/react/outline';
 import { supabase } from '../../utils/supabase-client.js';
 import EditOutletForm from '../../components/forms/EditOutletForm.js';
 import { useModals } from '@mantine/modals';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 OutletsDashboardPage.getLayout = (page) => {
     return <SidebarLayout>{page}</SidebarLayout>;
@@ -38,8 +42,12 @@ export default function OutletsDashboardPage() {
     const [outlets, setOutlets] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    const [outletSales, setOutletSales] = useState([]);
+
     useEffect(() => {
         const fetchOutlets = async () => {
+            if (!initialized) return;
+
             try {
                 setLoading(true);
 
@@ -58,7 +66,48 @@ export default function OutletsDashboardPage() {
         };
 
         fetchOutlets();
-    }, []);
+    }, [initialized]);
+
+    useEffect(() => {
+        const fetchOutletSales = async () => {
+            if (!initialized) return;
+            if (!outlets && !outlets.length) return;
+
+            const { data, error } = await supabase
+                .from('bill_products')
+                .select('amount, bills (outlet_id), products (price)');
+
+            if (error) throw error;
+
+            const outletSales = data.reduce((acc, curr) => {
+                const outlet = acc.find(
+                    (outlet) => outlet.id === curr.bills.outlet_id
+                );
+
+                const outletName = outlets.find(
+                    (outlet) => outlet.id === curr.bills.outlet_id
+                )?.name;
+
+                if (!outlet) {
+                    acc.push({
+                        id: curr.bills.outlet_id,
+                        name: outletName,
+                        sales: curr.amount,
+                        earnings: curr.amount * curr.products.price,
+                    });
+                } else {
+                    outlet.sales += curr.amount;
+                    outlet.earnings += curr.amount * curr.products.price;
+                }
+                return acc;
+            }, []);
+
+            console.log(outletSales);
+            setOutletSales(outletSales);
+        };
+
+        fetchOutletSales();
+    }, [initialized, outlets]);
 
     const addOutlet = async (outlet) => {
         try {
@@ -108,8 +157,105 @@ export default function OutletsDashboardPage() {
             onClose: () => {},
         });
 
+    const getDistinctOutletNames = () => {
+        if (!outletSales) return [];
+
+        return outletSales.reduce((acc, curr) => {
+            if (!acc.includes(curr.name)) {
+                acc.push(curr.name);
+            }
+            return acc;
+        }, []);
+    };
+
     return initialized ? (
         <div className="p-4 md:p-8 lg:p-16">
+            {outletSales && (
+                <>
+                    <Title label="Insights" className="mb-4" />
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                        <div className="max-w-sm">
+                            <Pie
+                                data={{
+                                    labels: getDistinctOutletNames(),
+                                    datasets: [
+                                        {
+                                            label: 'Sales',
+                                            data: outletSales.map(
+                                                (outlet) => outlet.sales
+                                            ),
+                                            backgroundColor: [
+                                                'rgba(54, 162, 235, 0.2)',
+                                                'rgba(255, 99, 132, 0.2)',
+                                                'rgba(255, 206, 86, 0.2)',
+                                                'rgba(75, 192, 192, 0.2)',
+                                                'rgba(153, 102, 255, 0.2)',
+                                                'rgba(255, 159, 64, 0.2)',
+                                            ],
+                                            borderColor: [
+                                                'rgba(54, 162, 235, 1)',
+                                                'rgba(255, 99, 132, 1)',
+                                                'rgba(255, 206, 86, 1)',
+                                                'rgba(75, 192, 192, 1)',
+                                                'rgba(153, 102, 255, 1)',
+                                                'rgba(255, 159, 64, 1)',
+                                            ],
+                                            borderWidth: 1,
+                                        },
+                                    ],
+                                }}
+                            />
+                            <div className="mt-2 mb-4 text-2xl text-center font-semibold">
+                                Most popular outlets
+                            </div>
+                        </div>
+
+                        <div className="max-w-sm">
+                            <Pie
+                                data={{
+                                    labels: getDistinctOutletNames(),
+                                    datasets: [
+                                        {
+                                            label: 'Sales',
+                                            data: outletSales.map(
+                                                (outlet) => outlet.earnings
+                                            ),
+                                            backgroundColor: [
+                                                'rgba(54, 162, 235, 0.2)',
+                                                'rgba(255, 99, 132, 0.2)',
+                                                'rgba(255, 206, 86, 0.2)',
+                                                'rgba(75, 192, 192, 0.2)',
+                                                'rgba(153, 102, 255, 0.2)',
+                                                'rgba(255, 159, 64, 0.2)',
+                                                'rgba(255, 99, 132, 0.2)',
+                                                'rgba(255, 206, 86, 0.2)',
+                                                'rgba(75, 192, 192, 0.2)',
+                                            ],
+                                            borderColor: [
+                                                'rgba(54, 162, 235, 1)',
+                                                'rgba(255, 99, 132, 1)',
+                                                'rgba(255, 206, 86, 1)',
+                                                'rgba(75, 192, 192, 1)',
+                                                'rgba(153, 102, 255, 1)',
+                                                'rgba(255, 159, 64, 1)',
+                                                'rgba(255, 99, 132, 1)',
+                                                'rgba(255, 206, 86, 1)',
+                                                'rgba(75, 192, 192, 1)',
+                                            ],
+                                            borderWidth: 1,
+                                        },
+                                    ],
+                                }}
+                            />
+                            <div className="mt-2 mb-4 text-2xl text-center font-semibold">
+                                Top earning outlets
+                            </div>
+                        </div>
+                    </div>
+                    <div className="my-8" />
+                </>
+            )}
+
             <div className="flex mb-4">
                 <Title label="Outlets" />
                 <button
